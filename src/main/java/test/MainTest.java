a63748c261a898e20d4f2459f7face926b13e323
msalu <msalumaa@gmail.com>
2021-02-18 14:00:48 +0200

    Testing AnswerRepository

--- src/main/java/test/MainTest.java
+++ src/main/java/test/MainTest.java
@@ -13,17 +13,28 @@ public class MainTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        Question question = new Question("What is Java?");
-//        QuestionRepository questionRepository = new QuestionRepository();
-//        questionRepository.save(question);
-//        Answer answer1 = new Answer("a", true, question);
-//        Answer answer2 = new Answer("b", false, question);
+       /* Question question = new Question("What is Java?");
+        QuestionRepository questionRepository = new QuestionRepository();
+        questionRepository.save(question);
+
+        Answer answer1 = new Answer("a", true, question);
+        Answer answer2 = new Answer("b", false, question);*/
+
+        /*Question question2 = new Question("What is SQL?");
+        QuestionRepository questionRepository = new QuestionRepository();
+        questionRepository.save(question2);
+        Answer answer1 = new Answer("f", true, question2);
+        Answer answer2 = new Answer("g", false, question2);
+        Answer answer3 = new Answer("h", false, question2);*/
+
 
         AnswerRepository answerRepository = new AnswerRepository();
 
-//        answerRepository.save(answer1);
-//        answerRepository.save(answer2);
+     /*   answerRepository.save(answer1);
+        answerRepository.save(answer2);
+        answerRepository.save(answer3);*/
 
-        answerRepository.answersWhereQuestionIdIsSame(1).forEach(a -> System.out.println(a.toString()));
+        // ar = answerRepository
+        answerRepository.answersWhereQuestionIdIsSame(2).forEach(ar -> System.out.println(ar.toString()));
     }
 }

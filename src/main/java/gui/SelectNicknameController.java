83c3bf7c5bd1b1ca1e974c7d5eb312c61d13bd90
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-02-19 18:06:45 +0200

    fixed nickname window

--- src/main/java/gui/SelectNicknameController.java
+++ src/main/java/gui/SelectNicknameController.java
@@ -1,15 +1,16 @@
 package gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.Player;
 import persistance.PlayerRepository;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,21 +23,29 @@ public class SelectNicknameController implements Initializable {
     private Stage stage = new Stage();
 
     @FXML
-    private TextField inputTxt;
+    private TextField nickname;
 
-    public void onClick(){
-        String nickname = inputTxt.getText();
-        player = new Player(nickname);
-        playerRepository = new PlayerRepository();
-        playerRepository.save(player);
 
+    @FXML
+    public void onClick(ActionEvent event){
         try {
-            scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("gui/questionScene.fxml")));
+            Node source = (Node) event.getSource();
+            stage = (Stage) source.getScene().getWindow();
+            stage.close();
+            scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("gui/startMenu.fxml")));
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void saveOnClick(){
+        String name = nickname.getText();
+        player = new Player(name);
+        playerRepository = new PlayerRepository();
+        playerRepository.save(player);
     }
 
     @Override
